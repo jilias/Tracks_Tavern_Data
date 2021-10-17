e420fe536b087b82a153991ddd89a1e0bf436bcf
Aica Mangilit <rica637@yahoo.com>
2021-10-17 00:42:10 -0400

    added revenue, group_dict pikle and pairing function in webmodel

--- model.py
+++ model.py
@@ -7,6 +7,10 @@ import pickle as p
 data_path = './ML_Model/'
 data_file = 'sales.pkl'
 model_file = 'linear.pkl'
+group_file = 'group_dict.pkl'
+revenue_file = 'revenue.pkl'
+type_file = 'individual_type.pkl'
+
 
 def load_data():
     global df
@@ -17,6 +21,16 @@ def load_model():
     global loaded_model
     loaded_model = p.load(open(data_path+'linear.pkl','rb'))
     return loaded_model
+
+def load_group():
+    global loaded_model
+    loaded_group = p.load(open(data_path+'group_dict.pkl','rb'))
+    return loaded_group
+
+def load_revenue():
+    global loaded_revenue
+    loaded_revenue = p.load(open(data_path+'revenue.pkl','rb'))
+    return loaded_revenue
 
 
 
